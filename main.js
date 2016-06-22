316d1a21e98d98d19c587daedfe4e96f57d09817
Will Johnson <williamterryjohnsonjr@gmail.com>
2016-06-21 20:20:55 -0400

    Edited main.js to toggle positions of ul elements via their class, edited main.scss to provide open and resting classes for each ul and added positioning for container, cleaned up class designations in index.html.

--- main.js
+++ main.js
@@ -3,10 +3,26 @@ var magicAppear = function(event) {
 	var target = event.target;
 	console.log(target);
 	if ($(target).hasClass("section1Header")){
-			$(".section1Text").toggleClass("disappear").toggleClass("appear");
-			if("")
+		if ($(".section2").hasClass("section2Open")){
+		$(".section2").toggleClass("section2Open");
 		}
+		if ($(".section3").hasClass("section3Open")){
+			$(".section3").toggleClass("section3Open");
+		}
+	} else if ($(target).hasClass("section2Header")){
+		if (!$(".section2").hasClass("section2Open")){
+		$(".section2").toggleClass("section2Open");
+		}
+		if ($(".section3").hasClass("section3Open")){
+			$(".section3").toggleClass("section3Open");
+		} 
+	} else if ($(target).hasClass("section3Header")){
+		if (!$(".section3").hasClass("section3Open")){
+		$(".section3").toggleClass("section3Open");
+		}
+		if (!$(".section2").hasClass("section2Open")){
+			$(".section2").toggleClass("section2Open");
+		}
+	}
 };
 $(".sectionHead").on('click', magicAppear);
-
-// $("class").addClass("appear").removeClass("disappear") - will cause the accordeon to open the way you want.
